4bdfa518642deac724130619bca48453a213c9a9
ldalakishvili <dalaqishvililevani9@gmail.com>
2026-09-14 10:31:14 +0400

    changing baseurl

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,56 +1,56 @@
 import axios from "axios";
 
 export const endpoints = {
-    Autorization: "Autorization",
-    GetProdNashti: "GetProdNashti",
-    GetProductList: "Prod/Prod_GetProductList",
-    GetProdGroup: "GetProdGroup",
-    GetProduct: "GetProduct",
-    GetProductPicture: "GetProductPicture",
-    GetStore: "GetStore",
-    GetSalaro: "GetSalaro",
-    GetMdzgoli: "GetMdzgoli",
-    SaveOrder: "mark4/operations/SaveOrder",
-    PayOrders: "mark4/operations/PayOrders",
-    PayTerminal: "mark4/TbcTerminal/Payment",
-    CloseDay : "mark4/TbcTerminal/CloseDay"
-}
+  Autorization: "Autorization",
+  GetProdNashti: "GetProdNashti",
+  GetProductList: "Prod/Prod_GetProductList",
+  GetProdGroup: "GetProdGroup",
+  GetProduct: "GetProduct",
+  GetProductPicture: "GetProductPicture",
+  GetStore: "GetStore",
+  GetSalaro: "GetSalaro",
+  GetMdzgoli: "GetMdzgoli",
+  SaveOrder: "mark4/operations/SaveOrder",
+  PayOrders: "mark4/operations/PayOrders",
+  PayTerminal: "mark4/TbcTerminal/Payment",
+  CloseDay: "mark4/TbcTerminal/CloseDay",
+};
 
-
-const url = `https://fmg.mark4.ge/api/`;
+// const url = `https://fmg.mark4.ge/api/`;
+const url = `https://10.10.220.1/`;
 
 const axiosAutorization = axios.create({
   baseURL: url,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // axios instance
 const axiosInstance = axios.create({
-    baseURL: url,
-    headers: {
-      'Accept': 'application/json',
-      "Accept-Encoding": "gzip, deflate",
-      "Accept-Language": "en-US,en;q=0.9",
-    },
+  baseURL: url,
+  headers: {
+    Accept: "application/json",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+  },
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('markToken');
-      if (token != null) {
-        config.headers.Authorization = token;
-      }
-      return config;
-    },
-    (err) => Promise.reject(err),
+  (config) => {
+    const token = localStorage.getItem("markToken");
+    if (token != null) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err),
 );
 
 axiosInstance.interceptors.response.use(
   (resp) => {
-    if(resp.data.ErrorMessage && resp.data.ErrorMessage.ErrorCode == -300){
-      window.location.pathname = "/"
+    if (resp.data.ErrorMessage && resp.data.ErrorMessage.ErrorCode == -300) {
+      window.location.pathname = "/";
     }
     return resp;
   },
@@ -59,9 +59,11 @@ axiosInstance.interceptors.response.use(
 
 // axios operation
 const axiosOperationInstance = axios.create({
-  baseURL: "https://fmg.mark4.ge/",
+  // baseURL: "https://fmg.mark4.ge/",
+  baseURL: "https://10.10.220.1/",
+
   headers: {
-    'Accept': 'application/json',
+    Accept: "application/json",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
   },
@@ -69,7 +71,7 @@ const axiosOperationInstance = axios.create({
 
 axiosOperationInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('markToken');
+    const token = localStorage.getItem("markToken");
     if (token != null) {
       config.headers.Authorization = token;
     }
@@ -77,5 +79,5 @@ axiosOperationInstance.interceptors.request.use(
   },
   (err) => Promise.reject(err),
 );
-  
+
 export { axiosInstance, axiosAutorization, axiosOperationInstance };
