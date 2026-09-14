6d8843ce370ea0b0d72dd59a72d85c4e7b003271
ldalakishvili <dalaqishvililevani9@gmail.com>
2026-09-14 11:54:34 +0400

    fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -17,7 +17,7 @@ export const endpoints = {
 };
 
 // const url = `https://fmg.mark4.ge/api/`;
-const url = `https://10.10.220.1/`;
+const url = `https://10.10.220.1/api`;
 
 const axiosAutorization = axios.create({
   baseURL: url,
